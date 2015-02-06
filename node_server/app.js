b37550548433b0c37d98e8eec60ed813c74b4146
Sardor Isakov <sisardor@gmail.com>
2015-02-06 14:46:23 -0800

    sequalize implementation

--- node_server/app.js
+++ node_server/app.js
@@ -1,8 +1,10 @@
 var express = require("express"),
-    path = require("path"),
-    forever = require('forever'),
-    ejs = require('ejs'),
-    Navigation = require('./controllers/navigation');
+		path = require("path"),
+		forever = require('forever'),
+		ejs = require('ejs'),
+		Navigation = require('./controllers/navigation');
+var models = require("./models");
+
 
 // Easier namespacing for routes
 require('express-namespace');
@@ -17,44 +19,71 @@ app.set('view engine', 'ejs');
 
 // Configure the app
 app.configure(function() {
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(path.join(__dirname, "public")));
-    app.use(express.errorHandler({
-        dumpExceptions: true,
-        showStack: true
-    }));
+		app.use(express.bodyParser());
+		app.use(express.cookieParser());
+		app.use(express.methodOverride());
+		app.use(app.router);
+		app.use(express.static(path.join(__dirname, "public")));
+		app.use(express.errorHandler({
+				dumpExceptions: true,
+				showStack: true
+		}));
 });
 
 app.use(app.router);
 app.use(function(req, res, next){
-  res.status(404);
+	res.status(404);
 
-  // respond with html page
-  if (req.accepts('html')) {
-    res.render('404', { url: req.url });
-    return;
-  }
+	// respond with html page
+	if (req.accepts('html')) {
+		res.render('404', { url: req.url });
+		return;
+	}
 
-  // respond with json
-  if (req.accepts('json')) {
-    res.send({ error: 'Not found' });
-    return;
-  }
+	// respond with json
+	if (req.accepts('json')) {
+		res.send({ error: 'Not found' });
+		return;
+	}
 
-  // default to plain-text. send()
-  res.type('txt').send('Not found');
+	// default to plain-text. send()
+	res.type('txt').send('Not found');
 });
 
 // Set up public resource server
 app.use('/public', express.static(__dirname + '/public'));
 
 // Site
-app.get('/', Navigation.index);
-// app.get('/category/:category/:page', Navigation.category);
-// app.get('/details/:id', Navigation.details);
+app.get('/', function (req, res) {
+
+	models.User.findAll({
+		include: [ models.Task ]
+	}).then(function(users) {
+		res.send({
+			title: 'Express',
+			users: users
+		});
+	});
+		//res.render('home', {featuredAds: null});
+});
+
+app.get('/example', function (req, res, next) {
+	console.log('response will be sent by the next function ...')
+	next()
+}, function (req, res) {
+
+	models.User
+	  .create({
+	    username: 'john-doe'
+	  })
+	  .complete(function(err, user) {
+	    /* ... */
+	    console.log(user)
+	    res.send('Hello from B!');
+	  })
+	
+});
+
 
 // Create server
 app.listen(3000, "127.0.0.1");
